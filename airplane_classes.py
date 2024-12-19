e8ebf476d97d2356611e80975483da9eac2b7343
ligotommy <116342243+ligotommy@users.noreply.github.com>
2024-12-19 23:38:47 +0200

    Added joystick for input

--- airplane_classes.py
+++ airplane_classes.py
@@ -1,6 +1,6 @@
 import math
-
 import pygame
+import pygame.gfxdraw
 import time
 
 AIRPLANE_FILE = "airplane.png"
@@ -22,39 +22,41 @@ class Airplane:
         self.image = None
         self.original_image = None
 
-    def update_pos(self, up, down, left, right, dtime):
-        forward = right if self.flipped else left
-        backwards = left if self.flipped else right
-        new_tilt_dir = backwards - forward
-        if not new_tilt_dir:
-            new_tilt_dir = up - down
+    def update_pos(self, direction, dtime):
+        angle = math.atan2(direction[1], direction[0])
+        if not any(direction):
+            new_tilt_dir = 0
+        elif 3*math.pi/4 <= angle <= math.pi or -math.pi <= angle < -math.pi/4:
+            new_tilt_dir = -1
+        else:
+            new_tilt_dir = 1
         if self.tilt_dir != new_tilt_dir:
             self.tilt_time = time.time()
             self.tilt_dir = new_tilt_dir
 
-        acc_direction = [right-left, up-down]
+        acc_direction = direction
         if self.velo[0] > 0 and acc_direction[0] == 0:
             self.velo[0] += -10 * self.acceleration * dtime
             self.velo[0] = max(0, self.velo[0])
-        if self.velo[0] < 0 and acc_direction[0] == 0:
+        elif self.velo[0] < 0 and acc_direction[0] == 0:
             self.velo[0] += 10 * self.acceleration * dtime
             self.velo[0] = min(0, self.velo[0])
         else:
-            self.velo[0] += 10 * acc_direction[0] * self.acceleration * dtime
+            self.velo[0] += 10 * math.copysign(1, acc_direction[0]) * self.acceleration * dtime
 
         if self.velo[1] > 0 and acc_direction[1] == 0:
             self.velo[1] += -10 * self.acceleration
             self.velo[1] = max(0, self.velo[1])
-        if self.velo[1] < 0 and acc_direction[1] == 0:
+        elif self.velo[1] < 0 and acc_direction[1] == 0:
             self.velo[1] += 10 * self.acceleration * dtime
             self.velo[1] = min(0, self.velo[1])
         else:
-            self.velo[1] += 10 * acc_direction[1] * self.acceleration * dtime
+            self.velo[1] += 10 * math.copysign(1, acc_direction[1]) * self.acceleration * dtime
 
-        self.velo[0] = min(self.max_velocity, self.velo[0])
-        self.velo[0] = max(-self.max_velocity, self.velo[0])
-        self.velo[1] = min(self.max_velocity, self.velo[1])
-        self.velo[1] = max(-self.max_velocity, self.velo[1])
+        self.velo[0] = min(self.max_velocity*abs(acc_direction[0]), self.velo[0])
+        self.velo[0] = max(-self.max_velocity*abs(acc_direction[0]), self.velo[0])
+        self.velo[1] = min(self.max_velocity*abs(acc_direction[1]), self.velo[1])
+        self.velo[1] = max(-self.max_velocity*abs(acc_direction[1]), self.velo[1])
 
         self.pos[0] += self.velo[0] * dtime
         self.pos[1] += self.velo[1] * dtime
@@ -80,3 +82,62 @@ class Airplane:
             self.image = pygame.transform.rotate(self.original_image, self.tilt_deg)
         pos = self.pos[0] - self.image.get_width() / 2, surface.get_height() - self.pos[1] - self.image.get_height() / 2
         surface.blit(self.image, pos)
+
+
+class Joystick:
+    def __init__(self, center: pygame.Vector2, radius, small_circle_radius,
+                 circle_color=(50, 50, 50, 150), small_circle_color=(0, 0, 0)):
+        self.center = center
+        self.radius = radius
+        self.small_circle_radius = small_circle_radius
+        self.small_circle_center = pygame.Vector2(0, 0)
+        self.circle_color = circle_color
+        self.small_circle_color = small_circle_color
+
+        self.is_held = False
+
+    def update(self, event: pygame.event.Event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            cursor_position = pygame.Vector2(event.dict["pos"])
+            max_distance = self.radius - self.small_circle_radius
+            distance = cursor_position.distance_to(self.center)
+            if distance < max_distance:
+                self.small_circle_center = cursor_position - self.center
+                self.is_held = True
+            elif distance < 1.3*self.radius:
+                self.small_circle_center = (cursor_position - self.center).normalize()*max_distance
+                self.is_held = True
+        if event.type == pygame.MOUSEMOTION:
+            cursor_position = pygame.Vector2(event.dict["pos"])
+            max_distance = self.radius - self.small_circle_radius
+            distance = cursor_position.distance_to(self.center)
+            if self.is_held:
+                if distance < max_distance:
+                    self.small_circle_center = cursor_position - self.center
+                    self.is_held = True
+                else:
+                    self.small_circle_center = (cursor_position - self.center).normalize()*max_distance
+                    self.is_held = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.is_held = False
+            self.small_circle_center = pygame.Vector2(0, 0)
+
+    def get_vector(self) -> pygame.Vector2:
+        vector = pygame.Vector2(self.small_circle_center.x, -self.small_circle_center.y)/self.radius
+        return vector
+
+    def draw(self, surface: pygame.Surface):
+        alpha_surface = pygame.Surface(surface.get_size(), flags=pygame.SRCALPHA)
+
+        # pygame.draw.circle(alpha_surface, self.circle_color, self.center, self.radius)
+        # pygame.draw.circle(alpha_surface, self.small_circle_color,
+        #                    self.small_circle_center+self.center, self.small_circle_radius)
+        pygame.gfxdraw.aacircle(alpha_surface, int(self.center.x), int(self.center.y), self.radius, self.circle_color)
+        pygame.gfxdraw.filled_circle(alpha_surface,  int(self.center.x), int(self.center.y), self.radius, self.circle_color)
+        small_circle_position = self.center + self.small_circle_center
+        pygame.gfxdraw.aacircle(alpha_surface,  int(small_circle_position.x), int(small_circle_position.y),
+                                self.small_circle_radius, self.small_circle_color)
+        pygame.gfxdraw.filled_circle(alpha_surface, int(small_circle_position.x), int(small_circle_position.y),
+                                     self.small_circle_radius, self.small_circle_color)
+
+        surface.blit(alpha_surface, (0, 0))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from airplane_classes import Airplane
+from airplane_classes import Airplane, Joystick
 
 ESCAPE_KEY = 27
 ARROW_UP_KEY = 1073741906
@@ -12,7 +12,8 @@ ARROW_RIGHT_KEY = 1073741903
 pygame.init()
 WINDOW_SIZE = pygame.display.Info().current_w, 500  # pygame.display.Info().current_h
 surface = pygame.display.set_mode(WINDOW_SIZE)
-airplane = Airplane([WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2], [0, 0], 300, 500, 300)
+airplane = Airplane([WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2], [0, 0], 1000, 1000, 300)
+joystick = Joystick(pygame.Vector2(100, surface.get_height()-100), 50, 20)
 background_color = 100, 130, 200
 
 clock = pygame.time.Clock()
@@ -50,9 +51,16 @@ while True:
                 holding_right = False
             if event_key == ARROW_LEFT_KEY:
                 holding_left = False
+        if (event.type == pygame.MOUSEBUTTONDOWN or
+                event.type == pygame.MOUSEBUTTONUP or
+                event.type == pygame.MOUSEMOTION):
+            joystick.update(event)
     surface.fill(background_color)
 
     dtime = clock.tick()/1000
-    airplane.update_pos(holding_up, holding_down, holding_left, holding_right, dtime)
+    # direction = pygame.Vector2(holding_right-holding_left, holding_up-holding_down)
+    direction = joystick.get_vector()
+    airplane.update_pos(direction, dtime)
     airplane.draw(surface, dtime)
+    joystick.draw(surface)
     pygame.display.update()
